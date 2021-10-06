4f0c277e8a7a9ce7d65a82ada9ed00c1c77aef8c
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-10-06 20:47:01 -0300

    update

---
+++ aula_01/js/pratica.js
@@ -0,0 +1,8 @@
+var nome = prompt("Digite seu nome: ");
+var raca = prompt("Digite sua raça: ");
+var classe = prompt("DIgite sua classe: ");
+alert("Concluido com sucesso")
+
+document.writeln("<h2>Personagem</h2>");
+document.writeln("<b>Nome:</b> " + nome);
+
--- aula_01/js/script.js
+++ aula_01/js/script.js
@@ -1,4 +1,4 @@
-var idade = 10000.00;
+var idade;
 var nomePersonagem = "Gandalf";
 var classe = "Mago";
 
@@ -8,3 +8,20 @@ var npc = "Seu madruga";
 var soma = 5 + 5 ;
 console.log(soma);
  
+var text1 = "Bruno";
+var text2 = " ";
+var text3 = "Cesar";
+
+var nome = text1 + text2 + text3;
+
+idade = 22;
+
+if (idade<18) {
+    alert("*MENOR DE IDADE*");
+}else{
+    alert("*MAIOR DE IDADE*")
+}
+
+usuario = prompt("Digite seu nome: ");
+
+alert(usuario);
