5fb8355219b8053f806ffa9730955e5a594b2563
Marcel0Henrique <marcelomonteiro1275@gmail.com>
2021-10-29 10:07:07 -0300

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-var nome = document.getElementById("nome").value;
-var lista = nome.slice(",");
-
-
-function teste() {
-    var info = document.getElementById("info");
-    var calc = Number( lista[0] + lista[1] ) + 6;
-    info.innerHTML = calc;
-}
