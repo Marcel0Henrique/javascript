b596106819326e6fd03fee24e55f28c8656f1270
Marcel0Henrique <marcelomonteiro1275@gmail.com>
2021-10-29 08:09:32 -0300

    update

---
+++ prova/Prova02.js
@@ -0,0 +1,6 @@
+var nome = document.getElementById("nome");
+
+function pedido() {
+    var c1 = document.getElementById("c1");
+    c1.innerHTML = nome.value;
+}
--- prova/assets/js/script.js
+++ prova/assets/js/script.js
@@ -3,14 +3,22 @@ var bebida = document.getElementById("inputBebidas");
 var formaPagamento = document.getElementById("inputPagamento");
 var nome = document.getElementById("inputNome");
 var sobrenome = document.getElementById("inputSobrenome");
+var rua = document.getElementById("inputRua");
+var numero = document.getElementById("inputNumero");
 
 
 function processamento(){
     var calc = Number(proteina.value) + Number(bebida.value);
     var pagamento = document.getElementById("formaPagamento");
     var valorTotal = document.getElementById("valorTotal");
-    
+    var infoclt = document.getElementById("infoCliente");
+
+
     valorTotal.innerHTML =  "R$ " + calc;
-    pagamento.innerText =  formaPagamento.value;
+    pagamento.innerHTML =  formaPagamento.value;
+    infoclt.innerHTML = nome.value + " " + sobrenome.value + ", " + "Rua " + rua.value + ", " + numero.value;
+
+
+
     
 }
