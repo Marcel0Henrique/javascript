f0e6052993fef847c88543268fc3785bcf830407
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2021-11-09 23:47:02 -0300

    Update script.js

--- aula_3/formulario/js/script.js
+++ aula_3/formulario/js/script.js
@@ -1,10 +1,17 @@
 var nome = document.getElementById("inputNome");
-
+var email = document.getElementById("inputEmail");
+var celular = document.getElementById("inputCelular");
 
 function vazio() {
-    if (nome.value === "") {
-        alert("Erro");
-    }else{
-        window.location.href = "http://www.w3schools.com";
+    if (nome.value == "") {
+        alert("Nome está vazio");
+    }else if(email.value == ""){
+        alert("Email está vazio");
+    }else if(celular.value == ""){
+        alert("Celular está vazio");
+    }
+    
+    else{
+        window.location.href = "";
     }
 }
